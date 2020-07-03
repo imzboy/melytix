696b3fbd79b72e91ce250980b6323512f8e0d518
MaxTeslya <powerfulltime@yandex.ru>
2020-07-03 14:30:06 +0300

    added some endponts

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, request
+
+from flask_restful import Resource, Api
+
+
+import user as User
 
 app = Flask(__name__)
+app.secret_key = b"\x92K\x1a\x0e\x04\xcc\x05\xc8\x1c\xc4\x04\x98\xef'\x8e\x1bC\xd6\x18'}:\xc1\x14"
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+api = Api(app)
 
-if __name__ == '__main__':
-    app.run()
+
+class HelloView(Resource):
+    def get(self):
+        return {'Message': 'Hello World!'}
+
+
+class RegistrationView(Resource):
+    """The registration endpoint.
+       Takes credentials and creates a new user.
+       Credentials : email, password"""
+
+    def post(self):
+        email = request.json['email']
+        password = request.json['password']
+        if not User.get_by_email(email):
+            User.register(email, password)
+            return {'status': 'success',
+                    'email': email}
+        else:
+            return {'Error': 'User with that email already created'}
+
+
+class LoginView(Resource):
+    def post(self):
+        email = request.json['email']
+        password = request.json['password']
+        verify = User.verify_password(email, password)
+        if verify and verify != 404:
+            return {'token': User.get_or_create_token(email)}
+        elif verify == 404:
+            return {'Error': 'user not found'}, 404
+        else:
+            return {'Error': 'wrong password'}
+
+
+# URLs declaring
+api.add_resource(HelloView, '/', methods=['GET'])
+api.add_resource(RegistrationView, '/registration/', methods=['POST'])
+api.add_resource(LoginView, '/login/', methods=['POST'])
---
+++ user.py
@@ -0,0 +1,101 @@
+import pymongo
+import os
+from hashlib import pbkdf2_hmac
+import binascii
+
+# Connecting to Mogodb Atlas
+collection = pymongo.MongoClient(
+    'mongodb+srv://MaxTeslya:7887334Mna@melytixdata'
+    '-ryedw.mongodb.net/test?retryWrites=true&w=majority')
+
+db = collection.MelytixUsers.Users
+
+
+def get_by_email(email: str):
+    user = db.find_one({'email': email})
+    if user:
+        return user
+    return None
+
+
+def get_by_id(user_id):
+    user = db.find_one({'_id': user_id})
+    if user:
+        return user
+    return None
+
+
+def register(email: str, password: str) -> None:
+    salt = os.urandom(24)
+    password = pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    db.insert_one({
+        'email': email,
+        'password': password,
+        'salt': salt
+    })
+
+
+def verify_password(email, inputted_pass):
+    user = db.find_one({'email': email})
+    if user:
+        salt = user['salt']
+        inputted_pass = pbkdf2_hmac(
+            'sha256',
+            inputted_pass.encode('utf-8'),
+            salt,
+            100000)
+        if user['password'] == inputted_pass:
+            return True
+        else:
+            return False
+    else:
+        return 404
+
+
+def get_or_create_token(email):
+    user = db.find_one({'email': email})
+    if user['auth_token']:
+        return user['auth_token']
+    else:
+        token = binascii.hexlify(os.urandom(20)).decode()
+        db.find_one_and_update(
+            {'email': email},
+            {'$set': {
+                'auth_token': token
+            }},
+            upsert=False
+        )
+        return token
+
+
+def add_scopes(email: str, scope: list):
+    """adding scopes for google apis in the database for future usage
+
+    Args:
+        email (str): the email that we use to find the user
+        scope (list): the scopes that we are adding
+    """
+    db.find_one_and_update(
+        {'email': email},
+        {'$set': {
+            'SCOPE': scope
+        }},
+        upsert=False
+    )
+
+
+def insert_tokens(email: str, access_token: str, refresh_token: str):
+    """Mongodb find adn update func for adding user tokens in db
+
+    Args:
+        email: the email that we use to find the user
+        access_token: the google access token
+        refresh_token: the google refresh token"""
+    db.find_one_and_update(
+        {'email': email},
+        {'$set': {
+            'tokens': {'g_access_token': access_token,
+                        'g_refresh_token': refresh_token}
+        }},
+        upsert=False
+    )
