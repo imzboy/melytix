68580c0b8db5bc69ab74de69a187b9cacb70248a
maxt <sistemkatsl@gmail.com>
2020-07-27 12:57:55 +0300

    changed redirect uris

--- Systems/Google/GoogleAuth.py
+++ Systems/Google/GoogleAuth.py
@@ -10,7 +10,9 @@ def code_exchange(code: str):
         'code': code,
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
-        'redirect_uri': 'https://kraftpy.github.io',
+        'redirect_uri': ['https://kraftpy.github.io',
+                         'http://localhost:8080',
+                         'http://127.0.0.1:8080'],
         'grant_type': 'authorization_code'
         }
 
