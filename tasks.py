819082b2712c8df95ccb7ddf6364482bc0c45b2d
imzboy <powerfulltime@yandex.ru>
2020-09-22 10:44:17 +0300

    fixed an circular import

--- tasks.py
+++ tasks.py
@@ -1,22 +1,12 @@
 import os
 import celery
 
-from app import app
 
 celery_app = celery.Celery(
-    app.import_name,
+    'melytix-celery',
     backend=os.environ['CELERY_RESULT_BACKEND'],
     broker=os.environ['BROKER_URL']
 )
-celery_app.conf.update(app.config)
-
-class ContextTask(celery.Task):
-    def __call__(self, *args, **kwargs):
-        with app.app_context():
-            return self.run(*args, **kwargs)
-
-celery.Task = ContextTask
-
 
 @celery_app.task
 def test():
