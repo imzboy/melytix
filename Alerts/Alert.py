14e6f9b2289fb3f17668292a905ff98dc8b74cdd
MaxTeslya <powerfulltime@yandex.ru>
2020-10-07 11:07:41 +0300

    added date created to alerts

--- Alerts/Alert.py
+++ Alerts/Alert.py
@@ -1,5 +1,7 @@
 import uuid
 
+import datetime
+
 class Alert:
     def __init__(self, category: str, title: str, description: str,
                 analytics_func):
@@ -8,15 +10,17 @@ class Alert:
         self.title = title
         self.description = description
         self.analytics_func = analytics_func
+        self.created_at = datetime.datetime.now().strftime('%d.%m.%Y')
 
     def generate(self) -> dict:
         """
-        Constructs it self in dict format to be ready to be insertred in the database
+        Constructs it self in dict/json format to be ready to be insertred in the database
         """
         return {
             "id": self._id,
             "category": self.category,
             "title": self.title,
             "description": self.description,
+            "created_at": self.created_at,
             "active": True
         }
