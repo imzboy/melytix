0543b6da8bcb05a9a9008c1051e64a18e396b257
RoG <panamboor@gmail.com>
2020-11-20 13:41:58 +0300

    add first testAlert

--- Alerts/Alerts.py
+++ Alerts/Alerts.py
@@ -15,7 +15,7 @@ def crytical_low_ga_users(metrics: dict):
     min_ga_users = ga_users[0]
     for i in range(len(ga_users)):
         if min_ga_users+(min_ga_users/100*30)<ga_users[i]:
-            min_ga_users=ga_users[i]
+            min_fga_users=ga_users[i]
 
     if min_ga_users != ga_users[0]:
         return True
@@ -155,7 +155,31 @@ path_to_low_ga_users_alert = Alert(
 #
 # Cancel of new alerts Vova 24.10.2020
 
+
+# New alerts Borya 19.11.2020
+#
+#
+def CtrOfAllSCuser(metrics:dict):
+    ctr = metrics.get('sc_ctr')
+    i = 1
+    for i in range(len(ctr)):
+        if ctr != ctr[i]:
+            return False
+        return True
+
+ctrOfAllSearchConsoleUser = Alert(
+    category='SEO',
+    title = f'CTR меньше 1 процента, люди не кликают на ваш сниппет в поиске!',
+    description = f'На протяжении недели CTR всех ваших объявлений достигает меньше одного процента - поменяйте заголов и описание сниппета в поиске ( изменив <title> и <description> страницы )',
+    analytics_func=CtrOfAllSCuser
+)
+#
+#
+# Cancel of new alerts Borya 19.11.2020
+
+
+
 def return_alerts():
     return [lower_than_yesterday, always_alert, crytical_low_ga_users_alert,
     crytical_high_ga_users_alert, crytical_day_ga_users_alert, path_to_grow_ga_users_alert,
-    path_to_low_ga_users_alert]
+    path_to_low_ga_users_alert,ctrOfAllSearchConsoleUser]
