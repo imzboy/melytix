192d698f46e8712fe39832af3151733d1e62f88a
maxt <max.t@gowombat.team>
2021-02-25 12:44:15 +0200

    added metrics to main

--- app.py
+++ app.py
@@ -96,9 +96,12 @@ def create_app():
 
                 if connected_systems.get('google_analytics'):
                     try:  # TODO: for now coz it can return a list
+                        connected_systems['google_analytics']['metrics'] = list(request.user.metrics.get('google_analytics').keys())
                         connected_systems['google_analytics']['filters'] = list(request.user.metrics.get('google_analytics').get('ga_sessions').keys())
                     except:
                         print('nope')
+                if connected_systems.get('search_console'):
+                    connected_systems['search_console']['metrics'] = list(request.user.metrics.get('search_console').keys())
 
 
             return {**connected_systems}, 200
