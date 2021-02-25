4857356bd6e4a03e8f9a2335619ffb9aeb647ad0
maxt <max.t@gowombat.team>
2021-02-25 09:42:47 +0200

    added resourses

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ from Systems.SiteParser.views import parser_bp
 def create_app():
     app = Flask(__name__)
     app.secret_key = b"\x92K\x1a\x0e\x04\xcc\x05\xc8\x1c\xc4\x04\x98\xef'\x8e\x1bC\xd6\x18'}:\xc1\x14"
-    app.config['CORS_HEADERS'] = 'Content-Type'
     APP_ENV = os.environ.get('APP_ENV', 'Dev')
     config = getattr(settings, f'{APP_ENV}Config')
     app.config.update(config.as_dict())
 
     api = Api(app)
 
-    cors = CORS(app)
+    cors = CORS(app, resources={r"*": {"origins": "*"}})
+
 
     app.register_blueprint(api_bp)
     app.register_blueprint(admin)
