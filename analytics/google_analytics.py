0dc07870697d2558ce8860195e856e2b24a0c2cc
Vlad Magdysh <v.magdysh@gmail.com>
2021-02-28 13:49:12 +0200

    Transferred tips 0.03

--- analytics/google_analytics.py
+++ analytics/google_analytics.py
@@ -2,6 +2,7 @@ from analytics.base import MetricAnalyzer, MetricNotFoundException, Tip, Alert
 
 import datetime
 
+
 class GaSessionsAnalyzer(MetricAnalyzer):
 
     def __init__(self, metrics: dict):
@@ -9,7 +10,7 @@ class GaSessionsAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_sessions not found')
 
-    def sessions_lower(self, alg_id):
+    def sessions_lower(self, alg_id) -> Alert:
         if (all_sessions := self.metric.get('total')):
             if len(all_sessions) > 1:
                 if all_sessions[-1] < all_sessions[-2]:
@@ -27,7 +28,7 @@ class GaUsersAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_users not found')
 
-    def critical_low_ga_users(self, alg_id):
+    def critical_low_ga_users(self, alg_id) -> Alert:
         if (ga_users := self.metric.get('total', [])[-7:]):
             min_ga_users = min(ga_users)
             average_users = sum(ga_users) - min_ga_users
@@ -41,7 +42,7 @@ class GaUsersAnalyzer(MetricAnalyzer):
                     title=f'В {day} день количество пользователей критически понизилось по сравнению с другими днями этой недели',
                     description='The test alert')
 
-    def critical_high_ga_users(self, alg_id):
+    def critical_high_ga_users(self, alg_id) -> Alert:
         if (ga_users := self.metric.get('total',[])[-7:]):
             max_ga_users = max(ga_users)
             average_users = sum(ga_users) - max_ga_users
@@ -55,7 +56,7 @@ class GaUsersAnalyzer(MetricAnalyzer):
                     title=f'В {day} день количество пользователей повысилось по сравнению с другими днями этой недели',
                     description='The test alert')
 
-    def path_to_grow_ga_users(self, alg_id):
+    def path_to_grow_ga_users(self, alg_id) -> Alert:
         if (ga_users := self.metric.get('total',[])[-7:]):
             if sorted(ga_users) == ga_users:
                 return Alert(
@@ -67,7 +68,7 @@ class GaUsersAnalyzer(MetricAnalyzer):
                                 'который был сделан в последние дни для того, чтобы повторить этот успех и усильте '
                                 'текущие активные маркетинговые каналы.')
 
-    def path_to_low_ga_users(self, alg_id):
+    def path_to_low_ga_users(self, alg_id) -> Alert:
         if (ga_users := self.metric.get('total')[-7:]):
             if sorted(ga_users, reverse=True) == ga_users:
                 return Alert(
@@ -79,6 +80,90 @@ class GaUsersAnalyzer(MetricAnalyzer):
                                 'выключенные или включенные рекламные каналы, а так же на показатель "Trust" вашего '
                                 'домена.')
 
+    def mobile_device_branding(self, alg_id) -> Tip:
+        if (all_mobile_devices := self.metric.get('ga_mobileDeviceBranding')):
+            main_brand_tuple = max(all_mobile_devices.items(), key=lambda x: sum(x[1][-7:]))  # [0] - key (brand name), [1] - data list
+            most_popular_brand = main_brand_tuple[0]
+            return Tip(
+                _id=alg_id,
+                category='Analytics, Целевая Аудитория',
+                title=f'Ваша целевая аудитория пользуется устройством {most_popular_brand}',
+                description=f'Ваша целевая аудитория пользуется устройством {most_popular_brand}. Внимательно просмотрите дизайн вашего '
+                            'сайта под модели мобильных устройств данного бренда. Так же, при настройках рекламных кампаний '
+                            'направляйте большую часть ( 60% ) рекламного бюджета на устройства данного типа.')
+
+    def browser_tip(self, alg_id) -> Tip:
+        if (all_browsers := self.metric.get('ga_browser')):
+            main_browser_tuple = max(all_browsers.items(), key=lambda x: sum(x[1][-7:]))  # [0] - key (browser name), [1] - data list
+            most_popular_browser = main_browser_tuple[0]
+            return Tip(
+                _id = alg_id,
+                category='Analytics, Целевая Аудитория',
+                title=f'Ваша целевая аудитория пользуется браузером - {most_popular_browser}',
+                description=f'Ваша целевая аудитория пользуется браузером - {most_popular_browser}. Внимательно просмотрите дизайн вашего сайта под этот браузер.')
+
+    def device_category_tip(self, alg_id) -> Tip:
+        if (all_categories := self.metric.get('ga_deviceCategory')):
+            if(all_sizes := self.metric.get('ga_browserSize')):
+                main_categories_tuple = max(all_categories.items(), key=lambda x: sum(x[1][-7:]))  # [0] - key (category name), [1] - data list
+                most_popular_category = main_categories_tuple[0]
+
+                main_device_size_tuple = max(all_sizes.items(), key=lambda x: sum(x[1][-7:]))  # [0] - key (size_str), [1] - data list
+                most_popular_size = main_device_size_tuple[0]
+
+                return Tip(
+                    _id = alg_id,
+                    category='Analytics, Целевая Аудитория',
+                    title=f'Ваша целевая аудитория пользуется девайсом -  {most_popular_category}',
+                    description=f'Ваша целевая аудитория пользуется девайсом -  {most_popular_category}. Проверьте адаптивность дизайна '
+                                'вашего сайта под данный тип устройства. Внимательно проверьте видимость и размер всех элементов для '
+                                f'данного размера типа устройства - {most_popular_size}. Заметьте, что на вашу конверсию может влиять, '
+                                'даже размер и цвет кнопки поэтому важные элементы вашей функциональности сайты должны быть выделены '
+                                'и видны для пользователя на данном типе устройства.')
+
+    def language_tip(self, alg_id) -> Tip:
+        if (all_languages := self.metric.get('ga_language')):
+            main_language_tuple = max(all_languages.items(), key=lambda x: sum(x[1][-7:]))  # [0] - key (language name), [1] - data list
+            total_sum = sum(sum(i[-7:]) for i in all_languages.values())
+            if float(sum(main_language_tuple[1]) / total_sum) > 0.8 :
+                set_of_languages = set(all_languages.keys())
+                set_of_languages.discard(main_language_tuple[0])
+                string_languages = str(set_of_languages)[1:-1]
+                str_main_language = main_language_tuple[0]
+                return Tip(
+                    _id = alg_id,
+                    category='Analytics, Целевая Аудитория',
+                    title=f'Ваша целевая аудитория общается не только на {str_main_language} ',
+                    description=f'Ваша целевая аудитория общается не только на {str_main_language}. '
+                                f'Заметьте, ваша основная целевая аудитория использует не только {str_main_language},'
+                                f' но ещё и {string_languages}. Создайте мультиязычную версию сайта или же проверьте '
+                                'её наличие и грамотный перевод. Это влияет на вашу конверсию!')
+
+    def interest_other_category_tip(self, alg_id) -> Tip:
+        if (all_interests := self.metric.get('ga_interestOtherCategory')):
+            str_interests = str(all_interests.keys())[11:-2]
+            return Tip(
+                _id = alg_id,
+                category='Analytics, Целевая Аудитория',
+                title='Используйте интересы вашей аудитории в настройках таргетированной рекламы в Facebook, Instagram!',
+                description='Melytix.ai провёл анализ Ваших пользователей и выявил важные интересы, которые относятся к вашей '
+                            f'целевой аудитории, а именно - {str_interests}. Используйте именно эти '
+                            'интересы для точного попадания в Вашу целевую аудиторию, а система проанализируют повышение Вашей '
+                            'конверсии после появления новых рекламных кампаний.')
+
+    def extra_city_tip(self, alg_id) -> Tip:
+        if (all_cities := self.metric.get('ga_city')):
+            sorted_cities_dict = dict(sorted(all_cities.items(), key=lambda x: sum(x[1][-7:]), reverse=True))
+            sorted_cities_names = sorted_cities_dict[:3].keys()
+            str_cities = str(sorted_cities_names)[11:-2]
+            return Tip(
+                _id = alg_id,
+                category='Analytics, Целевая Аудитория',
+                title=f'Основные города - {str_cities} дают Вам больше всего конверсий!',
+                description='Melytix.ai провёл анализ Ваших пользователей и выявил самые популярные города, которые дают Вам '
+                            'больше всего конверсий! Настройте точечные рекламные кампании с гипер - локаций на данные города, '
+                            'чтобы снизить стоимость лида вашей рекламной кампании и повысить ваши конверсии!')
+
 
 class GaBouncesAnalyzer(MetricAnalyzer):
 
@@ -87,7 +172,7 @@ class GaBouncesAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_bounces not found')
 
-    def ga_bounces_crytical_func(self, alg_id):
+    def ga_bounces_crytical_func(self, alg_id) -> Alert:
         if (week_ga_bounces_list := self.metric.get('total')[-7:]):
             weekly_sum = sum(week_ga_bounces_list)
             if weekly_sum / 7 > 50:
@@ -100,7 +185,7 @@ class GaBouncesAnalyzer(MetricAnalyzer):
                                 'сайте, чем возможно. Проверьте посадочные страницы на которые вы привлекаете трафик '
                                 'при первом касании с клиентом.')
 
-    def ga_bounces_bad_func(self, alg_id):
+    def ga_bounces_bad_func(self, alg_id) -> Alert:
         if (week_ga_bounces_list := self.metric.get('total')[-7:]):
             weekly_sum = sum(week_ga_bounces_list)
             if 25 < weekly_sum / 7 < 50:
@@ -113,7 +198,7 @@ class GaBouncesAnalyzer(MetricAnalyzer):
                                 'сайте, чем возможно. Проверьте посадочные страницы на которые вы привлекаете трафик '
                                 'при первом касании с клиентом.')
 
-    def ga_bounces_crytical_day_bad_func(self, alg_id):
+    def ga_bounces_crytical_day_bad_func(self, alg_id) -> Alert:
         if (week_ga_bounces_list := self.metric.get('total')[-7:]):
             for item in week_ga_bounces_list:
                 if item > 50:
@@ -134,7 +219,7 @@ class GaReturningUserAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_ReturningUser not found')
 
-    def ga_path_to_low_returning_user_func(self, alg_id):
+    def ga_path_to_low_returning_user_func(self, alg_id) -> Alert:
         if (returning_users := self.metric.get('total')[-7:]):
             if sorted(returning_users, reverse=True) == returning_users:
                 return Alert(
@@ -144,7 +229,7 @@ class GaReturningUserAnalyzer(MetricAnalyzer):
                     description='Похоже новые пользователи не возвращаются к Вам на сайт. Проверьте актуальность источников вашего ремаркетинга или ретаргетинга . Возврат пользователей это важная метрика Вашей конверсии и показатель ранжирования для поисковых систем. '
                                 'Обновите ваши креативы на ваших кампаниях нацеленных на возврат пользователей - скорее всего, они утратили актуальность.')
 
-    def ga_path_to_grow_returning_user_func(self, alg_id):
+    def ga_path_to_grow_returning_user_func(self, alg_id) -> Alert:
         if (returning_users := self.metric.get('total')[-7:]):
             if sorted(returning_users) == returning_users:
                 return Alert(
@@ -154,7 +239,7 @@ class GaReturningUserAnalyzer(MetricAnalyzer):
                     description='Отличная работа! Каналы вашего ремаркетинга показывают отличные показатели и растут за последние 7 дней возвращая'
                                 'всё больше и больше пользователей! Это позитивно складывается на вашей конверсии и ранжировании ваших сниппетов в поисковой системе.')
 
-    def low_ga_returning_user_func(self, alg_id):
+    def low_ga_returning_user_func(self, alg_id) -> Tip:
         if(weekly_returning := self.metric.get('total', [])[-7:]):
             average = sum(weekly_returning) / 7
             if 0.25 < average < 0.5:
@@ -167,7 +252,7 @@ class GaReturningUserAnalyzer(MetricAnalyzer):
                                 'возвраты трафика, а также просмотрите показатель “CTR” ваших объявлений для полного решения проблем '
                                 'в процессе возвращения вашего трафика.')
 
-    def no_ga_returning_user_func(self, alg_id):
+    def no_ga_returning_user_func(self, alg_id) -> Tip:
         if(weekly_returning := self.metric.get('total', [])[-7:]):
             average = sum(weekly_returning) / 7
             if average < 0.25:
@@ -182,7 +267,7 @@ class GaReturningUserAnalyzer(MetricAnalyzer):
                                 'ретаргетинг в социальных для возврата пользователей '
                                 'на сайт - это сможет повысить количество повторяющихся продаж, а также поднимет вашу конверсию.')
 
-    def right_ga_returning_user_func(self, alg_id):
+    def right_ga_returning_user_func(self, alg_id) -> Tip:
         if(weekly_returning := self.metric.get('total', [])[-7:]):
             average = sum(weekly_returning) / 7
             if average > 0.8:
@@ -201,7 +286,7 @@ class GaNewUserAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_NewUser not found')
 
-    def grows_of_new_users_func(self, alg_id):
+    def grows_of_new_users_func(self, alg_id) -> Tip:
         if(new_users := self.metric.get('total', [])[-7:]):
             if sorted(new_users) == new_users:
                 return Tip(
@@ -212,7 +297,7 @@ class GaNewUserAnalyzer(MetricAnalyzer):
                                 'настройки каналов маркетинга для дальнейшего повышения роста новых пользователей, который влияет но '
                                 'повышение показателя уникальной конверсии!')
 
-    def loss_of_new_users_func(self, alg_id):
+    def loss_of_new_users_func(self, alg_id) -> Tip:
         if(new_users := self.metric.get('total', [])[-7:]):
             if sorted(new_users, reverse=True) == new_users:
                 return Tip(
@@ -229,7 +314,7 @@ class GaPageViewsPerSessionAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_pageviewsPerSession not found')
 
-    def low_ga_page_views_per_session_func(self, alg_id):
+    def low_ga_page_views_per_session_func(self, alg_id) -> Tip:
         if(weekly_views := self.metric.get('total', [])[-7:]):
             for item in weekly_views:
                 if item < 1.5:
@@ -252,7 +337,7 @@ class GaAvgSessionDurationAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_avgSessionDuration not found')
 
-    def right_avg_session_duration_func(self, alg_id):
+    def right_avg_session_duration_func(self, alg_id) -> Tip:
         if(session_duration := self.metric.get('total', [])[-7:]):
             average = sum(session_duration) / 7
             if average > 1.3:
@@ -264,7 +349,7 @@ class GaAvgSessionDurationAnalyzer(MetricAnalyzer):
                                 ' контент на странице - для повышения данного показателя постарайтесь добавить больше пунктов навигации'
                                 ' для дальнейшего продолжения “изучения” пользователя вашего сайта. Хорошая работа!')
 
-    def low_avg_session_duration_func(self, alg_id):
+    def low_avg_session_duration_func(self, alg_id) -> Tip:
         if(session_duration := self.metric.get('total', [])[-7:]):
             average = sum(session_duration) / 7
             if 1.0 < average < 1.5:
@@ -276,7 +361,7 @@ class GaAvgSessionDurationAnalyzer(MetricAnalyzer):
                                 'главного домена в поисковике.Постарайтесь удержать внимание Вашего пользователя на сайте дольше, '
                                 'чем есть сейчас - добавив больше навигационных подсказок пути пользователя на вашей странице.')
 
-    def no_avg_session_duration_func(self, alg_id):
+    def no_avg_session_duration_func(self, alg_id) -> Tip:
         if(session_duration := self.metric.get('total', [])[-7:]):
             average = sum(session_duration) / 7
             if average < 1.0:
@@ -297,7 +382,7 @@ class GaAvgPageLoadTimeAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_avgPageLoadTime not found')
 
-    def critical_load_time(self, alg_id):
+    def critical_load_time(self, alg_id) -> Tip:
         week_slice = self.metric.get('total', [])[-7:]
         for item in week_slice:
             if item > 3:
@@ -311,7 +396,7 @@ class GaAvgPageLoadTimeAnalyzer(MetricAnalyzer):
                                 'конверсии. Постарайтесь сократить ненужный код на сайте, а так же кэшировать изображения на вашем '
                                 'сайте для улучшения данного показателя.')
 
-    def avg_load_time(self, alg_id):
+    def avg_load_time(self, alg_id) -> Tip:
         week_slice = self.metric.get('total', [])[-7:]
         for item in week_slice:
             if item > 2:
@@ -332,7 +417,7 @@ class GaTimeOnPageAnalyzer(MetricAnalyzer):
         if not self.metric:
             raise MetricNotFoundException('ga_timeOnPage not found')
 
-    def critical_time_on_page(self, alg_id):
+    def critical_time_on_page(self, alg_id) -> Tip:
         week_slice = self.metric.get('total', [])[-7:]
         for item in week_slice:
             if item < 10:
@@ -345,7 +430,7 @@ class GaTimeOnPageAnalyzer(MetricAnalyzer):
                                 'пользователю не понравился дизайн вашего сайта. Постарайтесь исправить хотя бы одно из этих событий, '
                                 'а Melytix.ai в автоматическом режиме снова измерит Ваши показатели!')
 
-    def low_time_on_page(self, alg_id):
+    def low_time_on_page(self, alg_id) -> Tip:
         week_slice = self.metric.get('total', [])[-7:]
         for item in week_slice:
             if item < 30:
@@ -357,3 +442,4 @@ class GaTimeOnPageAnalyzer(MetricAnalyzer):
                                 'несколько событий: на сайт приводится нерелевантный трафик, страницы сайта долго прогружаются. '
                                 'Постарайтесь исправить хотя бы одно из этих событий, а Melytix.ai в автоматическом режиме снова '
                                 'измерит Ваши показатели!')
+
