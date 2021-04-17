91198e18afa3a32c68327b9354c503fa9e68c76f
alexyounger-arch <56654914+alexyounger-arch@users.noreply.github.com>
2021-04-17 19:36:57 +0300

    Get db_name from configs

--- Utils/change_users_db.py
+++ Utils/change_users_db.py
@@ -1,6 +1,11 @@
+import os
 from user.base import client
+from config import settings
 
-db = client["melytix_db"]
+APP_ENV = os.environ.get('APP_ENV', 'Dev')
+config = getattr(settings, f'{APP_ENV}Config')
+db_name = config.DATABASE_NAME
+db = client[db_name]
 col = db["users"]
 
 col.update_many({}, {'$unset': {'Tips': 1, 'tips': 1, 'Alerts': 1, 'alerts': 1, 'metrics': 1}, '$set': {'language': 'en'}},
