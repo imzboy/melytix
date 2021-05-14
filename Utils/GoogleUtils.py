eee5ab3359ff69985f155fe8817a703445be718a
imzboy <powerfulltime@yandex.ru>
2021-05-14 22:03:04 +0300

    fixed an edge case with sc parser

--- Utils/GoogleUtils.py
+++ Utils/GoogleUtils.py
@@ -150,7 +150,7 @@ class GoogleReportsParser:
         return result
 
 
-def prep_dash_metrics(sc_data: dict) -> dict:
+def prep_dash_metrics(sc_data: dict, num_of_dates: int) -> dict:
     metrics = {
         'sc_dates': [],
         'sc_clicks': [],
@@ -159,13 +159,15 @@ def prep_dash_metrics(sc_data: dict) -> dict:
         'sc_position': [],
     }
     # TODO: refactor
-    for row in sc_data['rows']:
-        metrics['sc_dates'].append(row['keys'][0])
-        metrics['sc_clicks'].append(row.get('clicks', 0))
-        metrics['sc_impressions'].append(row.get('impressions', 0))
-        metrics['sc_ctr'].append(row.get('ctr', 0))
-        metrics['sc_position'].append(row.get('position', 0))
-
+    if sc_data.get('rows'):
+        for row in sc_data['rows']:
+            metrics['sc_dates'].append(row['keys'][0])
+            metrics['sc_clicks'].append(row.get('clicks', 0))
+            metrics['sc_impressions'].append(row.get('impressions', 0))
+            metrics['sc_ctr'].append(row.get('ctr', 0))
+            metrics['sc_position'].append(row.get('position', 0))
+    else:
+        metrics = {item: [0] * num_of_dates for item in metrics}
     return metrics
 
 
@@ -210,4 +212,3 @@ def fill_all_with_zeros(responce, dates):
             'total' : [metric['type'](0)] * len(dates)
         }
     return responce
-
